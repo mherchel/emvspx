81be14d535d98badb514bc395a7d1c153beb883e
Mike Herchel <mike@herchel.com>
2014-08-12 15:06:21 -0400

    Initial commit.

---
+++ px/config.rb
@@ -0,0 +1,27 @@
+# Require any additional compass plugins here.
+
+require 'sass-globbing'
+require 'breakpoint'
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "css"
+sass_dir = "sass"
+images_dir = "images"
+
+# You can select your preferred output style here (can be overridden via the command line):
+output_style = :expanded
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+relative_assets = true
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+line_comments = true
+
+
+# If you prefer the indented syntax, you might want to regenerate this
+# project again passing --syntax sass, or you can uncomment this:
+# preferred_syntax = :sass
+# and then run:
+# sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
+# sass_options = {:debug_info => true} # Enable this to use chrome tools to debug your sass
